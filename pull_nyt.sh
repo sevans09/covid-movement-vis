104c600c2ff1ea3defafa808af925d6b5b49ec7c
Sejal Dua <sejal.dua@tufts.edu>
2020-04-22 11:59:47 -0700

    fix bash script

--- pull_nyt.sh
+++ pull_nyt.sh
@@ -3,7 +3,10 @@ cd ../covid-19-data
 git pull
 cp us-counties.csv ../covid-movement-vis/data/cases-data/
 cd ../covid-movement-vis/data/cases-data
-python csv2json/csv2json.py us-counties.csv old-us-counties-cases.json
+python csv2json.py us-counties.csv old-us-counties-cases.json
 python gen_cases_data.py
 mv us-county-cases.json ../../
-head -c 50 us-county-cases.json
+cd ../../
+echo "Tail: "
+tail -c 54 us-county-cases.json
+echo "\n"
