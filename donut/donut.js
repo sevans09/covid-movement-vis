dfc780ef3a98478a9f772246ddb7ea560fa4c42e
Sejal Dua <sejal.dua@tufts.edu>
2020-04-24 23:40:18 -0700

    added specific keypress listeners that respond to separate enter events

---
+++ donut/donut.js
@@ -0,0 +1,22 @@
+function makeDonut(state) {
+    var donut = donutChart()
+        .width(300)
+        .height(300)
+        .cornerRadius(3) // sets how rounded the corners are on each slice
+        .padAngle(0.015) // effectively dictates the gap between slices
+        .variable('Relative Percentage')
+        .category('County');
+
+    d3.csv('donut/test.csv', function(error, data) {
+        newdata = data.filter(function(d) {
+            if (d.State == state) {
+                console.log(d);
+                return d;
+            }
+        })
+        if (error) throw error;
+        d3.select('#barchart')
+            .datum(newdata) // bind data to the div
+            .call(donut); // draw chart in div
+    });
+}
